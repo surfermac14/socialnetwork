60b16e738e2f614520387f5bc010a6a6689f5d30
abhinav <surveymac90@gmail.com>
2015-01-23 07:35:52 +0530

    Basic Registration completed

--- manage.py
+++ manage.py
@@ -4,20 +4,53 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField,PasswordField,RadioField
 from wtforms.validators import Required,Email
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'This is super strong secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
+
+# Models
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key= True)
+	fname = db.Column(db.String(30),nullable=False)
+	lname = db.Column(db.String(30),nullable=False)
+	email = db.Column(db.String(50),unique=True,nullable=False)
+	password = db.Column(db.String(50),nullable=False)
+	sex = db.Column(db.Boolean(),nullable=False)
+	
+	def __repr__(self):
+		return '<User %r' % self.fname
+
+		
+#Form Classes
 class RegistrationForm(Form):
 	fname = StringField(u'First Name',validators =[Required()])
 	lname = StringField(u'Last Name',validators=[Required()])
 	email = StringField(u'Email Id',validators=[Required(),Email()])
 	password = PasswordField(u'Password',validators=[Required()])
-	sex = RadioField(u'Sex',choices=[('Male','Male'),('Female','Female')],validators=[Required()])
+	sex = RadioField(u'Sex',choices=[('0','Male'),('1','Female')],validators=[Required()])
 	submit = SubmitField(u'Register')
+#Non Route functions
+def make_shell_context():
+	return dict(app=app,User=User)
 
+
+manager.add_command('db',MigrateCommand)
+
+#route functions
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -29,8 +62,15 @@ def user(name):
 @app.route('/registration',methods=['GET','POST'])
 def registration():
 	form = RegistrationForm()
+	user = User()
 	if form.validate_on_submit() :
-		
+		user.fname = form.fname.data
+		user.lname = form.lname.data
+		user.email  = form.email.data
+		user.password = form.password.data
+		user.sex = form.sex.data
+		db.session.add(user)
+		db.session.commit()
 		flash('Successfully registered. Please proceed to login page.')
 		return redirect(url_for('index'))
 	return render_template('registration.html',form=form)
