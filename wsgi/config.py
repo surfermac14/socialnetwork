62a81b3fe9c4bbc28e2e01133f1c82d7379bb253
abhinav <abhinav.m@thelogicpeople.com>
2015-02-06 15:35:48 +0530

    Initial deployment of flask app to web

--- wsgi/config.py
+++ wsgi/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 	SECRET_KEY = 'This is super strong secret key'
 	#SQLALCHEMY_DATABASE_URI ='sqlite:///'+os.path.join(basedir,'data.sqlite')
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:miriayala@localhost/socialnetwork'
+	SQLALCHEMY_DATABASE_URI = 'postgresql://admin8znz69x:TM8F9ck_CGz4@$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT/mysn'
 	#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'postgresql://postgres:miriyala@localhost/socialnetwork'
 	#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/socialnetwork'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
